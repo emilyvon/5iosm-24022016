c57bd6a6563ac2214aac7a36c90b10776edb6be0
Emily Fung <emilyfung@foxmail.com>
2016-02-24 10:25:59 +1030

    updates

--- 5iosm-24022016/ViewController.h
+++ 5iosm-24022016/ViewController.h
@@ -9,6 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (retain, nonatomic) IBOutlet UISlider *slider;
+@property (retain, nonatomic) IBOutlet UIImageView *img;
+- (IBAction)sliderValueChanged:(id)sender;
+@property (weak, nonatomic) IBOutlet UISlider *widthSlider;
+@property (weak, nonatomic) IBOutlet UISlider *heightSlider;
+- (IBAction)widthSliderValueChanged:(UISlider *)sender;
+
+- (IBAction)heightSliderValueChanged:(UISlider *)sender;
+
+@property (nonatomic) float myWidth;
+@property (nonatomic) float myHeight;
 
 
 @end
--- 5iosm-24022016/ViewController.m
+++ 5iosm-24022016/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+
+
+
 @end
 
 @implementation ViewController
@@ -17,11 +20,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    self.myWidth = 0;
+//    self.myHeight = 0;
+//    
+//    float imgWidth = self.img.frame.size.width;
+//    float imgHeight = self.img.frame.size.height;
+//    NSLog(@"%f", imgWidth);
+//    NSLog(@"%f", imgHeight);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+    
+//    float width, height;
+
+    NSLog(@"slider value: %f", sender.value);
+    
+    float sliderValue = sender.value;
+    self.img.frame = CGRectMake(138, 67, sliderValue, sliderValue);
+
 }
 
+
+- (IBAction)widthSliderValueChanged:(UISlider *)sender {
+    self.myWidth = sender.value;
+    self.img.frame = CGRectMake(138, 67, self.myWidth, self.img.frame.size.height);
+}
+
+- (IBAction)heightSliderValueChanged:(UISlider *)sender {
+    self.myHeight = sender.value;
+    self.img.frame = CGRectMake(138, 67, self.myWidth, self.myHeight);
+}
 @end
