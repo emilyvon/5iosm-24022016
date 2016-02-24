5bcf218efa5afff591af53f1d2693298d7ecbb64
Emily Fung <emilyfung@foxmail.com>
2016-02-24 11:55:08 +1030

    updates

--- 5iosm-24022016/SecondViewController.m
+++ 5iosm-24022016/SecondViewController.m
@@ -24,16 +24,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)sliderValueChanged:(UISlider *)sender {
     double monthlyRate = sender.value / 12;
     
@@ -41,6 +31,6 @@
     
     double repaymentAmt = self.principleAmtTxtField.text.doubleValue * (( monthlyRate * pow((1 + monthlyRate), self.yearsTxtField.text.doubleValue * 12)) / ( (pow((1 + monthlyRate), self.yearsTxtField.text.doubleValue * 12))-1));
     
-    self.repaymentAmtLbl.text = [NSString stringWithFormat:@"%f", repaymentAmt];
+    self.repaymentAmtLbl.text = [NSString stringWithFormat:@"Your monthly repayment is %f", repaymentAmt];
 }
 @end
